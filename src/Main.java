5f4580b9c475602994e4a8735084f83e45e1911f
tiophill1 <tiophill993@gmail.com>
2023-04-13 11:02:37 -0300

    criar dados enquanto, para e faça

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Laços enquanto - while
+        int contador = 0;
+        while (contador < 10) {
+            System.out.println("Contador = " + contador);
+            //Incremento
+            contador = contador + 1;
+        }
+        //laço do para faça - for
+        for (int x = 0; x < 10; x++) {
+            System.out.println("contador = " + x);
+        }
+        ///laço faça enquanto - do
+        int cont =1;
+        do{
+            //processo
+            System.out.println(cont);
+            cont++; //incremento
+            }while(cont <= 10);
     }
 }
