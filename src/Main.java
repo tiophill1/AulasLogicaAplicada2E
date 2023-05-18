cd11f909fe933b518018c05d85441e81f751dcb6
tiophill1 <tiophill993@gmail.com>
2023-05-18 12:26:43 -0300

    BOA

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        //Laços enquanto - while
-        int contador = 0;
-        while (contador < 10) {
-            System.out.println("Contador = " + contador);
-            //Incremento
-            contador = contador + 1;
-        }
-        //laço do para faça - for
-        for (int x = 0; x < 10; x++) {
-            System.out.println("contador = " + x);
-        }
-        ///laço faça enquanto - do
-        int cont =1;
-        do{
-            //processo
-            System.out.println(cont);
-            cont++; //incremento
-            }while(cont <= 10);
+        // Aula introdutoria de Orientação à Objetos
+        Pessoa adao = new Pessoa();
+        //declaração d ojeto
+        Pessoa qualquer;
+        // instanciação do obejeto
+        qualquer = new Pessoa();
+        //definição do formato da pessoa qualquer
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "lee";
+        qualquer.falar();
+
+
+
     }
-}
+
+ }
---
+++ src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+
+    // Atributos da pessoa
+
+    public String nome;
+    public String sobrenome;
+    // Metodos da classe
+    public void falar(){
+
+    }
+}
